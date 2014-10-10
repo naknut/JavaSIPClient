3a8b058ecf563ac3113a3d80c1ee3129d00af180
fredrikljung93 <frljung@kth.se>
2014-10-10 16:04:36 +0200

    Merge

--- src/Client.java
+++ src/Client.java
@@ -16,6 +16,9 @@ public class Client {
 
     static State state = new Idle();
     static String sipName;
+    static ServerSocket server;
+    static Thread t2;
+    static Thread t1;
 
     private static class InputHandler implements Runnable {
 
@@ -67,20 +70,41 @@ public class Client {
     }
 
     public static void main(String[] args) {
+        System.out.println("SIP Client started");
         sipName = args[1];
-        try {
-            ServerSocket server = new ServerSocket(Integer.parseInt(args[0]));
-            CallHandler handler = new CallHandler();
-           Thread handlerThread = new Thread(handler);
-            while (true) {
-                Socket socket = server.accept();
-                System.out.println("Server.accept()");
-               handlerThread.stop();
-                InputHandler inputHandler = new InputHandler(socket);
-               inputHandler.run();
+        ServerThread server=new ServerThread(Integer.parseInt(args[0]));
+        t1 = new Thread(server);
+        t1.start();
+        CallHandler callHandler= new CallHandler();
+        callHandler.run();
+        t2=new Thread(callHandler);
+        t1.start();
+
+    }
+
+    private static class ServerThread implements Runnable{
+        private int port;
+        public ServerThread(int port){
+            this.port=port;
+        }
+        @Override
+        public void run() {
+            try {
+                server = new ServerSocket(port);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            while (true) {
+                Socket socket = null;
+                try {
+                    socket = server.accept();
+                    t1.stop();
+                    InputHandler inputHandler = new InputHandler(socket);
+                    inputHandler.run();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
     }
 }
