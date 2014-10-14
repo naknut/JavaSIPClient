aa22871b64435212679d1fc34e9c75b7d808402d
fredrikljung93 <frljung@kth.se>
2014-10-14 11:37:41 +0200

    Now sending ACK after received 200 OK

--- src/States/Caller/WaitRinging.java
+++ src/States/Caller/WaitRinging.java
@@ -3,8 +3,11 @@ package States.Caller;
 import Audio.AudioStreamUDP;
 import States.BusyState;
 import States.Conversation;
+import States.Reciver.Ringing;
 import States.State;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -27,6 +30,14 @@ public class WaitRinging extends BusyState {
         if(input.startsWith("200 OK")) {
             String[] tokens = input.split(" ");
             InetAddress remoteAddress = ((InetSocketAddress)socket.getRemoteSocketAddress()).getAddress();
+            PrintWriter out;
+            try {
+                out = new PrintWriter(socket.getOutputStream(), true);
+                out.println("ACK");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             return new Conversation(socket, stream, remoteAddress, Integer.parseInt(tokens[2]),sipName);
         }
         else if(input.startsWith("INVITE"))
